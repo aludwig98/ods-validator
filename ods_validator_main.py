784dc62ef6862a8ec2643dff1a5595b45a041118
Andrew Ludwig <andrew.m.ludwig.civ@army.mil>
2022-04-07 19:38:21 +0100

    Update ods_validator_main.py

--- ods_validator_main.py
+++ ods_validator_main.py
@@ -113,6 +113,7 @@ def dispatch(environ, start_response):
 		
 
 if __name__ == '__main__':
-    listener = eventlet.listen(('0.0.0.0', os.environ.get("PORT", 80)))
+    print(f"Hosting on port: {int(os.environ.get('PORT', 80))}")
+    listener = eventlet.listen(('0.0.0.0', int(os.environ.get("PORT", 80))))
     print('\nVisit http://localhost:7000/ in your websocket-capable browser.\n')
     wsgi.server(listener, dispatch)
